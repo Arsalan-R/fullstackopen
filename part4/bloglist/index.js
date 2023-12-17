e6dc5b6039d18cce69c16ff2cd85c32d627b10c1
arsalan <arsalanroknidev@gmail.com>
2023-12-17 21:40:10 +0330

    4.2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+
 const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config();
-const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blogs')
+
+
 
 const unknownEndpoint = (req, res) => {
   res.status(404).send({error: 'unkown endpoint'});
@@ -15,23 +18,7 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogRouter)
 
 app.use(unknownEndpoint)
 
