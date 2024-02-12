161832df5460edb6e1f23a153058417ee3d024ea
arsalan <arsalanroknidev@gmail.com>
2024-02-12 04:07:52 +0330

    clean main

--- part6/redux-anecdotes-main/src/main.jsx
+++ part6/redux-anecdotes-main/src/main.jsx
@@ -1,19 +1,9 @@
 import ReactDOM from 'react-dom/client'
-import { configureStore } from '@reduxjs/toolkit'
 
-import filterReducer from './reducers/filterReducer'
 import { Provider } from 'react-redux'
 import App from './App'
-import aneReducer from './reducers/anecdoteReducer'
-import notificationReducer from './reducers/notificationReducer'
 
-const store = configureStore({
-  reducer: {
-    anecdote : aneReducer,
-    filter: filterReducer,
-    notification: notificationReducer
-  }
-})
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
